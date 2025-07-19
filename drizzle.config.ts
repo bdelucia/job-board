dce7037b0ac4d420973b9ac7f61a20e28dfb4fa2
bdelucia <bdelucia@asu.edu>
2025-07-18 18:47:08 -0700

    database script commands and migrations set up

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,9 @@
-import "dotenv/config";
+import { env } from "@/data/env/server";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./src/drizzle/schema.ts",
-  out: "./src/drizzle",
+  out: "./src/drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
