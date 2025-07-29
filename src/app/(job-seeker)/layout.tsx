f4220c2f8f516d63e70ccb823f88ef0119e56cff
Bobby DeLucia (endeavorOS) <bdelucia@asu.edu>
2025-07-29 10:03:07 -0700

    layout refactoring

---
+++ src/app/(job-seeker)/layout.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react"
+
+export default function JobSeekerLayout({ children }: { children: ReactNode }) {
+  return children
+}
--- src/components/sidebar/AppSidebar.tsx
+++ src/components/sidebar/AppSidebar.tsx
@@ -10,15 +10,14 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { AppSidebarClient } from "./_AppSidebarClient"
 import Link from "next/link"
 import { LogInIcon } from "lucide-react"
-import { SignedIn } from "@clerk/nextjs"
-import { Suspense } from "react"
 import { SignedOut } from "@/services/clerk/components/SignInStatus"
 import { SidebarUserButton } from "@/features/users/components/SidebarUserButton"
-import { AppSidebarClient } from "@/components/sidebar/_AppSidebarClient"
+import { ReactNode } from "react"
 
-export default function HomePage() {
+export function AppSidebar({ content }: { content: ReactNode }) {
   return (
     <SidebarProvider className="overflow-y-hidden">
       <AppSidebarClient>
@@ -27,22 +26,7 @@ export default function HomePage() {
             <SidebarTrigger />
             <span className="text-xl text-nowrap">Jobs by Bob</span>
           </SidebarHeader>
-          <SidebarContent>
-            <SidebarGroup>
-              <SidebarMenu>
-                <SignedOut>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton asChild>
-                      <Link href="/sign-in">
-                        <LogInIcon />
-                        <span>Log in</span>
-                      </Link>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                </SignedOut>
-              </SidebarMenu>
-            </SidebarGroup>
-          </SidebarContent>
+          <SidebarContent>{content}</SidebarContent>
           <SidebarFooter>
             <SidebarMenu>
               <SidebarMenuItem>
