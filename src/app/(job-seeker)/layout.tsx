b5c343c7bfbd5165f7aefcb52d53e3f143f7691a
Bobby DeLucia (endeavorOS) <bdelucia@asu.edu>
2025-08-11 17:19:32 -0700

    feature: sidebar job listing filtering form

--- src/app/(job-seeker)/layout.tsx
+++ src/app/(job-seeker)/layout.tsx
@@ -9,33 +9,42 @@ import {
 } from "lucide-react"
 import { ReactNode } from "react"
 
-export default function JobSeekerLayout({ children }: { children: ReactNode }) {
+export default function JobSeekerLayout({
+  children,
+  sidebar,
+}: {
+  children: ReactNode
+  sidebar: ReactNode
+}) {
   return (
     <AppSidebar
       content={
-        <SidebarNavMenuGroup
-          className="mt-auto"
-          items={[
-            { href: "/", icon: <ClipboardListIcon />, label: "Job Board" },
-            {
-              href: "/ai-search",
-              icon: <BrainCircuitIcon />,
-              label: "AI Search",
-            },
-            {
-              href: "/employer",
-              icon: <LayoutDashboard />,
-              label: "Employer Dashboard",
-              authStatus: "signedIn",
-            },
-            {
-              href: "/sign-in",
-              icon: <LogInIcon />,
-              label: "Sign In",
-              authStatus: "signedOut",
-            },
-          ]}
-        />
+        <>
+          {sidebar}
+          <SidebarNavMenuGroup
+            className="mt-auto"
+            items={[
+              { href: "/", icon: <ClipboardListIcon />, label: "Job Board" },
+              {
+                href: "/ai-search",
+                icon: <BrainCircuitIcon />,
+                label: "AI Search",
+              },
+              {
+                href: "/employer",
+                icon: <LayoutDashboard />,
+                label: "Employer Dashboard",
+                authStatus: "signedIn",
+              },
+              {
+                href: "/sign-in",
+                icon: <LogInIcon />,
+                label: "Sign In",
+                authStatus: "signedOut",
+              },
+            ]}
+          />
+        </>
       }
       footerButton={<SidebarUserButton />}
     >
