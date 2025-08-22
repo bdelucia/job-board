e53350d45df97b01e95601e3734f3462a8bb6862
Bobby DeLucia (endeavorOS) <bdelucia@asu.edu>
2025-08-22 14:47:23 -0700

    fix: neon postgres url env variable

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import { env } from "@/data/env/server";
-import { defineConfig } from "drizzle-kit";
+import { env } from "@/data/env/server"
+import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
   schema: "./src/drizzle/schema.ts",
   out: "./src/drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: env.POSTGRES_URL,
   },
-});
+})
